4ec1d300915e7f538aecb705243fbced5c84dd18
mooghoo <albertopablito8@gmail.com>
2023-03-04 20:48:07 -0700

    trying to use font :(

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+
+
 let color = "black";
 
 io.on("connection", (socket) => {
@@ -33,6 +35,8 @@ io.on("connection", (socket) => {
   });
 });
 
+app.use('/fonts', express.static('fonts'))
+
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: "./views" });
 });
@@ -44,6 +48,7 @@ app.get("/display", (req, res) => {
 app.get("/admin", (req, res) => {
   res.sendFile("admin.html", { root: "./views" });
 });
+
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
